71d65ace7e8d7863d59e25d6d71820851c34043e
Chase Gosingtian <c.gosingtian@klab.com>
2014-08-21 09:11:03 +0800

    Initial Commit

---
+++ UIElements/KLBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KLBAppDelegate.h
+//  UIElements
+//
+//  Created by Chase Gosingtian on 8/21/14.
+//  Copyright (c) 2014 KLab Cyscorpions, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KLBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UIElements/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UIElements
+//
+//  Created by Chase Gosingtian on 8/21/14.
+//  Copyright (c) 2014 KLab Cyscorpions, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "KLBAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([KLBAppDelegate class]));
+    }
+}
